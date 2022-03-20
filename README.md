dfb30b0c1e9c0b3e418633201b9e18a1b47603d6
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-03-20 00:57:41 -0600

    final commit

--- README.md
+++ README.md
@@ -6,14 +6,20 @@
 
 Cash register simulator.
 
-TODO: FINISH WRITING THIS!!!
+The purpose of this project is to create a simple cash register for someone in the world who desires to use java programming to determine how much change should be rendered to a person who is giving money in exchange for a product.
 
 ## Technologies/Skills Used
 
 * Java
 * Eclipse
-* TODO: MORE
+* Typing
+* Logical Skills
+* Brain Power
 
 ## Lessons Learned
 
-TODO: WRITE THIS
+This was a beneficial project because it helped me to identify issues in java when dealing with numerical calculations. One of the big issues I encountered was rounding errors. I sought to overcome this issue by manipulating the numerical data until I achieved the output that I wanted. 
+
+This project also highlighted challenges in formatting and representations of numbers in java. I still have much to learn regarding these two areas and am not entirely happy with the visual representation of the output in this program.
+
+One of the rewarding aspects of this project was discovering a pattern that could work in java code to achieve the desired results. The pattern I used could probably be simplified greatly with various techniques that I am not comfortable using yet, but I can see how valuable it is to identify patterns and simplify code using those patterns with more advanced techniques.
--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -8,27 +8,27 @@ public class CashRegister {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("What is the price of the item?");
+		System.out.print("What is the price of the item?");
 
 		double price = sc.nextDouble();
 
-		System.out.println("Price: $" + price);
+		System.out.println("$" + price);
 		price = price * 100;
 		int priceInt = (int) price;
 
-		System.out.println("How much money was tendered by the customer?");
+		System.out.print("How much money was tendered by the customer?");
 		double amtTendered = sc.nextDouble();
 
-		System.out.println("Amount tendered: $" + amtTendered);
+		System.out.println("$" + amtTendered);
 		amtTendered = amtTendered * 100;
 		int amtTenderedInt = (int) amtTendered;
 
 		int changeTotInt = (amtTenderedInt - priceInt);
 		double changeTot = (double) changeTotInt / 100;
-		System.out.println("Total change: $" + changeTot);
+		System.out.println("\n" + "Total change due: $" + changeTot);
 
 		if (amtTendered < price) {
-			System.out.println("Customer provided too little.");
+			System.out.println("Error: customer provided too little.");
 		}
 
 		if (amtTendered == price) {
@@ -37,71 +37,64 @@ public class CashRegister {
 
 		// make change
 		if (amtTendered > price) {
-			System.out.println("Give the customer the following change: ");
+			System.out.println("\n" + "Give the following change: ");
 
 			// twenties
 			int twenties = changeTotInt / 2000;
 
-			System.out.println("Twenty-dollar bill(s): " + twenties);
+			System.out.print("Twenties: " + twenties);
 
 			int newChangeAfterTwenties = changeTotInt - (twenties * 2000);
-			System.out.println("New change: " + newChangeAfterTwenties);
 
 			// tens
 			int tens = newChangeAfterTwenties / 1000;
 
-			System.out.println("Ten-dollar bill(s): " + tens);
+			System.out.print("\t Tens: " + tens);
 
 			int newChangeAfterTens = newChangeAfterTwenties - (tens * 1000);
-			System.out.println("New change: " + newChangeAfterTens);
 
 			// fives
 			int fives = newChangeAfterTens / 500;
 
-			System.out.println("Five-dollar bill(s): " + fives);
+			System.out.print("\t Fives: " + fives);
 
 			int newChangeAfterFives = newChangeAfterTens - (fives * 500);
-			System.out.println("New change: " + newChangeAfterFives);
 
 			// ones
 			int ones = newChangeAfterFives / 100;
 
-			System.out.println("One-dollar bill(s): " + ones);
+			System.out.println("\t Ones: " + ones + "\n");
 
 			int newChangeAfterOnes = newChangeAfterFives - (ones * 100);
-			System.out.println("New change: " + newChangeAfterOnes);
 
 			// quarters
 			int quarters = newChangeAfterOnes / 25;
 
-			System.out.println("Quarters: " + quarters);
+			System.out.print("Quarters: " + quarters);
 
 			int newChangeAfterQuarters = newChangeAfterOnes - (quarters * 25);
-			System.out.println("New change: " + newChangeAfterQuarters);
 
 			// dimes
 			int dimes = newChangeAfterQuarters / 10;
 
-			System.out.println("Dimes: " + dimes);
+			System.out.print("\t Dimes: " + dimes);
 
 			int newChangeAfterDimes = newChangeAfterQuarters - (dimes * 10);
-			System.out.println("New change: " + newChangeAfterDimes);
 
 			// nickels
 			int nickels = newChangeAfterDimes / 5;
 
-			System.out.println("Nickels: " + nickels);
+			System.out.print("\t Nickels: " + nickels);
 
 			int newChangeAfterNickels = newChangeAfterDimes - (nickels * 5);
-			System.out.println("New change: " + newChangeAfterNickels);
 
 			// pennies
 			int pennies = newChangeAfterNickels / 1;
 
-			System.out.println("Pennies: " + pennies);
+			System.out.print("\t Pennies: " + pennies);
 
 		}
-
+		
 		sc.close();
 	}
 }
